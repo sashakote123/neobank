8357c6a8c035bab0e3a2aa74b56e999df101f6ec
Alexander Kotikhin <sapool@bk.ru>
2025-06-19 17:07:58 +0300

    Переработал хуки на получение валют и новостей

--- src/hooks/useGetCurrency.ts
+++ src/hooks/useGetCurrency.ts
@@ -1,28 +1,79 @@
 import { useEffect, useState } from "react";
-import { CurrencyData } from "../types/types";
+import { CurrencyData, IImage } from "../types/types";
+
+import usd from "@images/currency/usd.svg";
+import chf from "@images/currency/chf.svg";
+import cny from "@images/currency/cny.svg";
+import eur from "@images/currency/eur.svg";
+import gbp from "@images/currency/gbp.svg";
+import jpy from "@images/currency/jpy.svg";
 
 interface HookResult {
-    data: CurrencyData | null,
-    loading: boolean,
-    error: boolean,
+  convertedData: any | null;
+  loading: boolean;
+  error: boolean;
 }
 
-export const useGetCurrency = (ref: string, apikey: string | undefined): HookResult => {
-    const [data, setData] = useState<CurrencyData | null>(null)
-    const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<boolean>(false)
+const imgArray: IImage[] = [
+  {
+    img: usd,
+    code: "usd",
+  },
+  {
+    img: chf,
+    code: "chf",
+  },
+  {
+    img: cny,
+    code: "cny",
+  },
+  {
+    img: eur,
+    code: "eur",
+  },
+  {
+    img: jpy,
+    code: "jpy",
+  },
+  {
+    img: gbp,
+    code: "gbp",
+  },
+];
 
-    useEffect(() => {
-        fetch(`${ref}${apikey}`)
-            .then(resp => {
-                if (resp.ok) return resp.json()
-                else throw new Error()
-            })
-            .catch(() => setError(true))
-            .then(json => { json ? setData(json.data) : setData(json) })
-            .finally(() => setLoading(false))
-    }, [apikey, ref])
+export const useGetCurrency = (): HookResult => {
+  const [data, setData] = useState<CurrencyData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [convertedData, setConvertedData] = useState<any[]>([]);
 
+  useEffect(() => {
+    fetch(
+      `https://api.currencyapi.com/v3/latest?apikey=${process.env.REACT_APP_CURRENCY_APIKEY}`
+    )
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        else throw new Error();
+      })
+      .catch(() => setError(true))
+      .then((json) => {
+        json ? setData(json.data) : setData(json);
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-    return { data, loading, error }
-}
+  useEffect(() => {
+    if (data) {
+      const roubleCourse = Number(data.RUB?.value.toFixed(3));
+      const newData = imgArray.map((item) => ({
+        img: item.img,
+        name: data[item.code.toUpperCase()]?.code || item.code,
+        value: +(roubleCourse / data[item.code.toUpperCase()]?.value).toFixed(
+          2
+        ),
+      }));
+      setConvertedData(newData);
+    }
+  }, [data]);
+  return { convertedData, loading, error };
+};
--- src/hooks/useGetNews.ts
+++ src/hooks/useGetNews.ts
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react";
 import { INews } from "../types/types";
 
-
 interface HookResult {
-    data: INews[] | null,
-    loading: boolean,
-    error: boolean,
+  data: INews[] | null;
+  loading: boolean;
+  error: boolean;
 }
 
-export const useGetNews = (ref: string, apikey: string | undefined): HookResult => {
-    const [data, setData] = useState<INews[] | null>([])
-    const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<boolean>(false)
+export const useGetNews = (): HookResult => {
+  const [data, setData] = useState<INews[] | null>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
-    useEffect(() => {
-        fetch(`${ref}${apikey}`)
-            .then(resp => {
-                if (resp.ok) return resp.json()
-                else throw new Error()
-            })
-            .catch(() => setError(true))
-            .then(json => json ? setData(json.articles) : setData(json))
-            .finally(() => setLoading(false))
-    }, [apikey, ref])
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.REACT_APP_NEWS_APIKEY}`
+    )
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        else throw new Error();
+      })
+      .catch(() => setError(true))
+      .then((json) =>
+        json ? setData(json.articles.slice(0, 10)) : setData(json)
+      )
+      .finally(() => setLoading(false));
+  }, []);
 
-
-    return { data, loading, error }
-}
+  return { data, loading, error };
+};
