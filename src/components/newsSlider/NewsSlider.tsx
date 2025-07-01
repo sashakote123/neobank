76bab1033225ad88ef6b6a00dcb1ea75d936dbb2
Alexander Kotikhin <sapool@bk.ru>
2025-07-01 14:46:59 +0300

    Несколько фиксов

--- src/components/newsSlider/NewsSlider.tsx
+++ src/components/newsSlider/NewsSlider.tsx
@@ -1,19 +1,19 @@
-import './styles.css';
-import { INews } from '@/src/types/types';
-import NewsItem from '../newsItem/NewsItem';
+import "./styles.css";
+import { INews } from "@/src/types/types";
+import NewsItem from "../newsItem/NewsItem";
 
 interface Props {
-    leftScroll: number;
-    newsArray: INews[];
+  leftScroll: number;
+  newsArray: INews[];
 }
 
 const NewsSlider: React.FC<Props> = ({ leftScroll, newsArray }) => {
-    return (
-        <ul style={{ marginLeft: `${leftScroll}px` }} className="news__slider">
-            {newsArray.map((item: INews, index: number) => {
-                return <NewsItem key={index} item={item} />;
-            })}
-        </ul>
-    );
+  return (
+    <ul style={{ marginLeft: `${leftScroll}px` }} className="news__slider">
+      {newsArray.map((item: INews, index: number) => {
+        return <NewsItem key={index} item={item} />;
+      })}
+    </ul>
+  );
 };
 export default NewsSlider;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,39 +1,36 @@
 export interface INews {
-    urlToImage: string,
-    url: string,
-    title: string,
-    description: string,
+  urlToImage: string;
+  url: string;
+  title: string;
+  description: string;
 }
 
 export interface ILink {
-    to: string,
-    text: string
+  to: string;
+  text: string;
 }
 
-
 export interface ICurrency {
-    img: string,
-    name: string,
-    value: number,
+  img: string;
+  name: string;
+  value: number;
 }
 
 export interface IImage {
-    img: string,
-    code: string
+  img: string;
+  code: string;
 }
-
 
 export interface CurrencyData {
-    [key: string]: {
-        code: string;
-        value: number;
-    };
+  [key: string]: {
+    code: string;
+    value: number;
+  };
 }
 
-
 export interface INews {
-    urlToImage: string,
-    url: string,
-    title: string,
-    description: string,
+  urlToImage: string;
+  url: string;
+  title: string;
+  description: string;
 }
