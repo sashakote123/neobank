7d52fd7dcfac31ac3ec50dd26d725d2bc9e74fd4
Alexander Kotikhin <sapool@bk.ru>
2025-07-15 15:27:20 +0300

    Переписал класс на clsx

--- src/features/loanNavigation/LoanNavigation.tsx
+++ src/features/loanNavigation/LoanNavigation.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router";
 import styles from "./styles.module.css";
 import { ILink } from "@/src/shared/types/types";
+import clsx from "clsx";
 
 const linksArray: ILink[] = [
   { to: "/card/about", text: "About card" },
@@ -9,21 +10,21 @@ const linksArray: ILink[] = [
   { to: "/card/faq", text: "FAQ" },
 ];
 
+type active = {
+  isActive: boolean;
+};
+
 const LoanNavigation = () => {
+  const setActive = ({ isActive }: active) =>
+    clsx([styles.itemLink], { [styles.linkActive]: isActive });
+
   return (
     <nav className={styles.container}>
       <ul className={styles.navigatinList}>
         {linksArray.map((item: ILink) => {
           return (
             <li key={item.to}>
-              <NavLink
-                to={item.to}
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.itemLink} ${styles.linkActive}`
-                    : styles.itemLink
-                }
-              >
+              <NavLink to={item.to} className={setActive}>
                 {item.text}
               </NavLink>
             </li>
