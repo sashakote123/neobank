5ad43c509b0d7c4ae8cfd259de551191808347b2
Alexander Kotikhin <sapool@bk.ru>
2025-07-22 21:07:32 +0300

    Исправил проверку возраста

--- src/shared/formSchema/formSchema.ts
+++ src/shared/formSchema/formSchema.ts
@@ -1,10 +1,20 @@
 import { z } from "zod";
 
 const isAdult = (dateString: string) => {
-  const birthDate = new Date(dateString);
-  const ageDifMs = Date.now() - birthDate.getTime();
-  const ageDate = new Date(ageDifMs);
-  return Math.abs(ageDate.getUTCFullYear() - 1970) >= 18;
+  const [day, month, year] = dateString.split(".");
+  const birthDate = new Date(`${year}-${month}-${day}`);
+  if (isNaN(birthDate.getTime())) return false;
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age >= 18;
 };
 
 export const formSchema = z.object({
