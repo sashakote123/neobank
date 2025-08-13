376d2148a69326e9222c1e8db3133b48fda6db44
Kotikhin Alex <sapool@bk.ru>
2025-08-13 21:33:11 +0300

    Вынес логику заполнения формы кода в отдельный хук

--- src/features/codeForm/CodeForm.tsx
+++ src/features/codeForm/CodeForm.tsx
@@ -1,11 +1,6 @@
-import { useParams } from 'react-router';
-
-import { useEffect, useRef, useState } from 'react';
-
-import { loanApi } from '@/src/shared/api/service';
-
 import circle from './assets/circle.svg';
 import loader from './assets/loader.svg';
+import useCodeForm from './hooks/useCodeForm';
 import styles from './styles.module.css';
 
 interface Props {
@@ -13,50 +8,8 @@ interface Props {
 }
 
 const CodeForm: React.FC<Props> = ({ setIsShowForm }) => {
-  const { applicationId } = useParams();
-  const [values, setValues] = useState<string[]>(['', '', '', '']);
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-
-  const [enterCode, { isError, isLoading, reset }] = loanApi.useEnterCodeMutation();
-
-  const handleInputChange = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value;
-    const digit = newValue.slice(-1);
-    const newValues = [...values];
-    newValues[index] = digit;
-    setValues(newValues);
-
-    if (digit && index < inputRefs.current.length - 1) {
-      inputRefs.current[index + 1]?.focus();
-    }
-  };
-
-  useEffect(() => {
-    if (values.join('').length === 4) {
-      const submitCode = async () => {
-        if (values.join('').length === 4) {
-          try {
-            await enterCode({ data: values, applicationId }).unwrap();
-            setIsShowForm(true);
-          } catch (error) {}
-        }
-      };
-
-      submitCode();
-    }
-  }, [applicationId, enterCode, setIsShowForm, values]);
-
-  const handleKeyDown = (index: number, event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (isError) reset();
-
-    if (event.key === 'Backspace' && !values[index] && index > 0) {
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
-
-  if (inputRefs.current.length !== values.length) {
-    inputRefs.current = Array(values.length).fill(null);
-  }
+  const { values, inputRefs, isLoading, isError, handleInputChange, handleKeyDown } =
+    useCodeForm(setIsShowForm);
 
   return (
     <form data-testid="codeForm" className={styles.formNumber}>
